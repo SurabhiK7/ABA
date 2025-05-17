e6f1c91a204a8b43dcc491ea4ca3fa442c5f0a11
surabhiK7 <surabhiksonu2004@gmail.com>
2025-05-18 03:15:59 +0530

    demo

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 public class add{
     public static void main(String[] args){
-        int a=7;
-        int b=5;
+        int a=9;
+        int b=11;
         int sum=a+b;
         System.out.println("Sum of "+a+" and "+b+" is : "+sum);
     }
