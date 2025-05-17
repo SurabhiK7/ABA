205ebd4590c60146ce8ab94312896e84c8db5233
unknown <surabhiksonu2004@gmail.com>
2025-05-17 23:37:19 +0530

    Trigger Travis CI build

--- add.java
+++ add.java
@@ -1,9 +1,3 @@
-// # git init
-// # git add .
-// # git commit -m "Initial commit"
-// # git branch -M main
-// # git remote add origin https://github.com/yourusername/my-cool-project.git
-// # git push -u origin main
 public class add{
     public static void main(String[] args){
         int a=7;
