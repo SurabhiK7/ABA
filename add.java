6848a8cf882bd1c50a831ff466d6a7d3f3953aa4
unknown <surabhiksonu2004@gmail.com>
2025-05-17 23:16:40 +0530

    initial commit

---
+++ add.java
@@ -0,0 +1,14 @@
+// # git init
+// # git add .
+// # git commit -m "Initial commit"
+// # git branch -M main
+// # git remote add origin https://github.com/yourusername/my-cool-project.git
+// # git push -u origin main
+public class add{
+    public static void main(String[] args){
+        int a=7;
+        int b=5;
+        int sum=a+b;
+        System.out.println("Sum of "+a+" and "+b+" is : "+sum);
+    }
+}
