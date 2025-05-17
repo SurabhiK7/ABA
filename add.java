649c9ccf154e80a46af4f2274b58dd21bc86fd1e
surabhiK7 <surabhiksonu2004@gmail.com>
2025-05-18 00:58:04 +0530

    Saving local changes before rebase

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 public class add{
     public static void main(String[] args){
-        int a=7;
-        int b=5;
+        int a=75;
+        int b=57;
         int sum=a+b;
         System.out.println("Sum of "+a+" and "+b+" is : "+sum);
     }
