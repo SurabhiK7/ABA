a62f3d94960d02c806f84ba4bf5f0d8b9de5f3fd
surabhiK7 <surabhiksonu2004@gmail.com>
2025-05-18 07:13:05 +0530

    commit

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 public class add{
     public static void main(String[] args){
-        int a=7;
-        int b=5;
+        int a=5;
+        int b=7;
         int sum=a+b;
         System.out.println("Sum of "+a+" and "+b+" is : "+sum);
     }
